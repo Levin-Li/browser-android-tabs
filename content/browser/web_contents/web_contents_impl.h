f3122e8d1335634d90ddb138515df95360a38911
samartnik <artem@brave.com>
2018-08-28 10:20:25 +0300

    Changed document referrer processing. Added referrer blocking whitelist.

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -1547,7 +1547,6 @@ class CONTENT_EXPORT WebContentsImpl : public WebContents,
   // changes.
   void UpdateVisibilityAndNotifyPageAndView(Visibility new_visibility);
 
-  void ShouldBlockReferrer(NavigationHandle* navigation_handle);
   // Data for core operation ---------------------------------------------------
 
   // Delegate for notifying our owner about stuff. Not owned by us.
